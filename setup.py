df4baf8297721e152ddd5c7252e2c464e5173b61
Juan Cruz Sosa <juancruzsosa.92@gmail.com>
2021-03-11 00:31:09 -0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(name='ml4data',
+      version='1.0',
+      description='ML4Data client library',
+      author='ML4Data Team',
+      author_email='info@ml4data.com',
+      url='https://ml4data.com/',
+      packages=find_packages(),
+      install_requires=required
+     )
