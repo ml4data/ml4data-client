3bafcb671f861414a29b6d83875b40a0d6db29d2
Juan Cruz Sosa <juancruzsosa.92@gmail.com>
2021-03-11 01:18:07 -0300

    Release v0.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='ml4data',
-      version='0.1',
+      version='0.1.1',
       description='ML4Data client library',
       author='ML4Data Team',
       author_email='info@ml4data.com',
